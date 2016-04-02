87b79e693226f1fb17d32e3c6b368bce9db276d4
kgozali <kgozali23@gmail.com>
2016-04-02 15:44:12 +0700

    gak jadi update

--- ekspedisi/Transaksi/transaksi_DO.vb
+++ ekspedisi/Transaksi/transaksi_DO.vb
@@ -1,12 +1,4 @@
 ﻿
 Public Class transaksi_DO
 
-
-    Private Sub GroupControl3_Paint(sender As Object, e As PaintEventArgs) Handles GroupControl3.Paint
-
-    End Sub
-
-    Private Sub GridControl1_Click(sender As Object, e As EventArgs) Handles GridControl1.Click
-        Command = New mysqlcommand
-    End Sub
 End Class
