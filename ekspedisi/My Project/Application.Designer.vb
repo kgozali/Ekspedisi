021164970da337cd0f470ef0df395900b0e60edc
Steve <setivess@gmail.com>
2016-04-02 22:56:28 +0700

    gatau ini apa yg dikomit

--- ekspedisi/My Project/Application.Designer.vb
+++ ekspedisi/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.ekspedisi.transaksi_DO
+            Me.MainForm = Global.ekspedisi.add_rekening
         End Sub
     End Class
 End Namespace
