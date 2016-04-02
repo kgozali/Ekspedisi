8740b09b3c0a2943f13fc30ea6635831022d1c79
Steve <setivess@gmail.com>
2016-04-02 21:53:17 +0700

    code(done)

--- ekspedisi/Add Master/add_user.vb
+++ ekspedisi/Add Master/add_user.vb
@@ -1,3 +1,28 @@
-﻿Public Class add_user 
+﻿Imports MySql.Data.MySqlClient
+Public Class add_user
+    Dim inputuser As String = ""
+    Private Sub cancel_Click(sender As Object, e As EventArgs) Handles cancel.Click
+        Me.Close()
+    End Sub
 
+    Private Sub simpan_Click(sender As Object, e As EventArgs) Handles simpan.Click
+        If password.Text = reenterpass.Text Then
+            inputuser = "insert into muser values ('" & username.Text & "',md5('" & password.Text & "')) "
+            InsertInto(inputuser)
+            MsgBox("Data Telah Terinput!")
+            add_user_Load(sender, e)
+        Else
+            MsgBox("Password tidak sama")
+            password.Text = ""
+            reenterpass.Text = ""
+        End If
+
+
+    End Sub
+
+    Private Sub add_user_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        username.Text = ""
+        password.Text = ""
+        reenterpass.Text = ""
+    End Sub
 End Class
