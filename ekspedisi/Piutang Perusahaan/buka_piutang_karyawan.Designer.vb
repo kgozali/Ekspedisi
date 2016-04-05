254f3720e0951d017add94942318036417ee391d
Januar <95goodyear@gmail.com>
2016-04-05 14:38:42 +0700

    piutang karyawan

--- ekspedisi/Piutang Perusahaan/buka_piutang_karyawan.Designer.vb
+++ ekspedisi/Piutang Perusahaan/buka_piutang_karyawan.Designer.vb
@@ -37,6 +37,8 @@ Partial Class buka_piutang_karyawan
         Me.GroupControl1 = New DevExpress.XtraEditors.GroupControl()
         Me.GroupControl2 = New DevExpress.XtraEditors.GroupControl()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.LabelControl7 = New DevExpress.XtraEditors.LabelControl()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
         CType(Me.pilihkaryawan.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.GroupControl1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.GroupControl1.SuspendLayout()
@@ -50,9 +52,9 @@ Partial Class buka_piutang_karyawan
         Me.LabelControl1.Appearance.Font = New System.Drawing.Font("Tahoma", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.LabelControl1.Location = New System.Drawing.Point(5, 28)
         Me.LabelControl1.Name = "LabelControl1"
-        Me.LabelControl1.Size = New System.Drawing.Size(147, 18)
+        Me.LabelControl1.Size = New System.Drawing.Size(164, 18)
         Me.LabelControl1.TabIndex = 34
-        Me.LabelControl1.Text = "ID Piutang Karyawan :"
+        Me.LabelControl1.Text = "Kode Piutang Karyawan :"
         '
         'LabelControl2
         '
@@ -92,14 +94,14 @@ Partial Class buka_piutang_karyawan
         '
         'id
         '
-        Me.id.Location = New System.Drawing.Point(165, 25)
+        Me.id.Location = New System.Drawing.Point(175, 25)
         Me.id.Name = "id"
         Me.id.Size = New System.Drawing.Size(162, 21)
         Me.id.TabIndex = 1
         '
         'pilihkaryawan
         '
-        Me.pilihkaryawan.Location = New System.Drawing.Point(165, 50)
+        Me.pilihkaryawan.Location = New System.Drawing.Point(175, 50)
         Me.pilihkaryawan.Name = "pilihkaryawan"
         Me.pilihkaryawan.Properties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton()})
         Me.pilihkaryawan.Size = New System.Drawing.Size(191, 20)
@@ -107,21 +109,21 @@ Partial Class buka_piutang_karyawan
         '
         'DateTimePicker1
         '
-        Me.DateTimePicker1.Location = New System.Drawing.Point(165, 73)
+        Me.DateTimePicker1.Location = New System.Drawing.Point(175, 73)
         Me.DateTimePicker1.Name = "DateTimePicker1"
         Me.DateTimePicker1.Size = New System.Drawing.Size(200, 21)
         Me.DateTimePicker1.TabIndex = 3
         '
         'nominal
         '
-        Me.nominal.Location = New System.Drawing.Point(165, 147)
+        Me.nominal.Location = New System.Drawing.Point(175, 147)
         Me.nominal.Name = "nominal"
         Me.nominal.Size = New System.Drawing.Size(200, 21)
         Me.nominal.TabIndex = 5
         '
         'DateTimePicker2
         '
-        Me.DateTimePicker2.Location = New System.Drawing.Point(165, 97)
+        Me.DateTimePicker2.Location = New System.Drawing.Point(175, 97)
         Me.DateTimePicker2.Name = "DateTimePicker2"
         Me.DateTimePicker2.Size = New System.Drawing.Size(200, 21)
         Me.DateTimePicker2.TabIndex = 4
@@ -129,7 +131,7 @@ Partial Class buka_piutang_karyawan
         'SimpleButton1
         '
         Me.SimpleButton1.Image = CType(resources.GetObject("SimpleButton1.Image"), System.Drawing.Image)
-        Me.SimpleButton1.Location = New System.Drawing.Point(568, 281)
+        Me.SimpleButton1.Location = New System.Drawing.Point(568, 290)
         Me.SimpleButton1.Name = "SimpleButton1"
         Me.SimpleButton1.Size = New System.Drawing.Size(100, 27)
         Me.SimpleButton1.TabIndex = 145
@@ -138,7 +140,7 @@ Partial Class buka_piutang_karyawan
         'Submit
         '
         Me.Submit.Image = CType(resources.GetObject("Submit.Image"), System.Drawing.Image)
-        Me.Submit.Location = New System.Drawing.Point(462, 281)
+        Me.Submit.Location = New System.Drawing.Point(462, 290)
         Me.Submit.Name = "Submit"
         Me.Submit.Size = New System.Drawing.Size(100, 27)
         Me.Submit.TabIndex = 144
@@ -156,13 +158,15 @@ Partial Class buka_piutang_karyawan
         'akunkas
         '
         Me.akunkas.FormattingEnabled = True
-        Me.akunkas.Location = New System.Drawing.Point(165, 121)
+        Me.akunkas.Location = New System.Drawing.Point(175, 121)
         Me.akunkas.Name = "akunkas"
         Me.akunkas.Size = New System.Drawing.Size(278, 21)
         Me.akunkas.TabIndex = 158
         '
         'GroupControl1
         '
+        Me.GroupControl1.Controls.Add(Me.RichTextBox1)
+        Me.GroupControl1.Controls.Add(Me.LabelControl7)
         Me.GroupControl1.Controls.Add(Me.akunkas)
         Me.GroupControl1.Controls.Add(Me.LabelControl1)
         Me.GroupControl1.Controls.Add(Me.LabelControl5)
@@ -177,7 +181,7 @@ Partial Class buka_piutang_karyawan
         Me.GroupControl1.Controls.Add(Me.pilihkaryawan)
         Me.GroupControl1.Location = New System.Drawing.Point(12, 9)
         Me.GroupControl1.Name = "GroupControl1"
-        Me.GroupControl1.Size = New System.Drawing.Size(450, 177)
+        Me.GroupControl1.Size = New System.Drawing.Size(450, 275)
         Me.GroupControl1.TabIndex = 159
         Me.GroupControl1.Text = "Data Piutang"
         '
@@ -186,7 +190,7 @@ Partial Class buka_piutang_karyawan
         Me.GroupControl2.Controls.Add(Me.PictureBox1)
         Me.GroupControl2.Location = New System.Drawing.Point(468, 9)
         Me.GroupControl2.Name = "GroupControl2"
-        Me.GroupControl2.Size = New System.Drawing.Size(200, 263)
+        Me.GroupControl2.Size = New System.Drawing.Size(200, 275)
         Me.GroupControl2.TabIndex = 160
         Me.GroupControl2.Text = "Bukti Piutang"
         '
@@ -194,15 +198,32 @@ Partial Class buka_piutang_karyawan
         '
         Me.PictureBox1.Location = New System.Drawing.Point(5, 24)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(190, 234)
+        Me.PictureBox1.Size = New System.Drawing.Size(190, 246)
         Me.PictureBox1.TabIndex = 0
         Me.PictureBox1.TabStop = False
+        '
+        'LabelControl7
+        '
+        Me.LabelControl7.Appearance.Font = New System.Drawing.Font("Tahoma", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LabelControl7.Location = New System.Drawing.Point(5, 174)
+        Me.LabelControl7.Name = "LabelControl7"
+        Me.LabelControl7.Size = New System.Drawing.Size(75, 18)
+        Me.LabelControl7.TabIndex = 159
+        Me.LabelControl7.Text = "Keterangan"
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(175, 174)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(270, 96)
+        Me.RichTextBox1.TabIndex = 160
+        Me.RichTextBox1.Text = ""
         '
         'buka_piutang_karyawan
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(678, 312)
+        Me.ClientSize = New System.Drawing.Size(678, 327)
         Me.Controls.Add(Me.GroupControl2)
         Me.Controls.Add(Me.SimpleButton1)
         Me.Controls.Add(Me.Submit)
@@ -236,4 +257,6 @@ Partial Class buka_piutang_karyawan
     Friend WithEvents GroupControl1 As DevExpress.XtraEditors.GroupControl
     Friend WithEvents GroupControl2 As DevExpress.XtraEditors.GroupControl
     Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents RichTextBox1 As System.Windows.Forms.RichTextBox
+    Friend WithEvents LabelControl7 As DevExpress.XtraEditors.LabelControl
 End Class
