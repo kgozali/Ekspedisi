3471a70ee888035c1ee298f00c8d057c165115b7
Kevin Gozali - DELL <kevingozali@streetlabsoftware.com>
2016-04-07 01:34:00 +0700

    tambahan

--- ekspedisi/Module1.vb
+++ ekspedisi/Module1.vb
@@ -3,6 +3,7 @@ Public Module Module1
     Public connect As New MySqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings("ekspedisi").ConnectionString)
 
     Function Scalar(ByVal x As String)
+        'untuk select single data
         Try
             connect.Open()
             Dim command As New MySqlCommand(x, connect)
@@ -16,6 +17,7 @@ Public Module Module1
     End Function
 
     Function DtTable(ByVal x As String)
+        'untuk select datatable biasa
         Try
             Dim command As New MySqlCommand(x, connect)
             command.Connection = connect
@@ -31,6 +33,7 @@ Public Module Module1
     End Function
 
     Function InsertInto(ByVal x As String)
+        'insert semua pakek ini
         Try
             connect.Open()
             Dim command As New MySqlCommand(x, connect)
@@ -42,6 +45,7 @@ Public Module Module1
     End Function
 
     Function DtTablemaster(ByVal x As String)
+        'select khusus master data yang perlu checkbox 
         Try
 
             Dim command As New MySqlCommand(x, connect)
@@ -63,6 +67,7 @@ Public Module Module1
     End Function
 
     Function auditlog(ByVal a As String, ByVal b As String, ByVal c As String, ByVal d As String, ByVal e As String, ByVal f As String)
+        'untuk insert auditlog, tinggal panggil trus lempar
         Try
             connect.Open()
             Dim Command As MySqlCommand = New MySqlCommand("insert into audit_log values('" + a + "','" + b + "','" + c + "','" + d + "','" + e + "','" + f + "')", connect)
