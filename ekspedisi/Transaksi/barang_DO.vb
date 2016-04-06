bb993948338bd6763be431ddb341a9197036ee48
Kevin Gozali - DELL <kevingozali@streetlabsoftware.com>
2016-04-06 18:47:34 +0700

    test

--- ekspedisi/Transaksi/barang_DO.vb
+++ ekspedisi/Transaksi/barang_DO.vb
@@ -3,4 +3,8 @@
     Private Sub GridControl1_Click(sender As Object, e As EventArgs) Handles GridControl1.Click
 
     End Sub
+
+    Private Sub barang_DO_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
