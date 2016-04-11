31fea12ebd229b3223c9d6403908c56790f6d0f2
Niko.Andriyas <nikoandriyas.mes@gmail.com>
2016-04-11 14:03:34 +0700

    maste_coa

--- ekspedisi/Master Data/master_akun.vb
+++ ekspedisi/Master Data/master_akun.vb
@@ -1,3 +1,41 @@
-﻿Public Class master_akun 
+﻿Public Class master_akun
 
+    Private Sub refreshgrid(ByVal keyid As String)
+        Dim lcdata As DataTable
+        If IsNothing(lcdata) = False Then lcdata.Rows.Clear()
+        lcdata = DtTable("select * from vw_makun where keyid like '%" & keyid & "%'")
+        GridControl1.DataSource = lcdata
+        GridControl1.Refresh()
+    End Sub
+
+    Private Sub master_akun_Load(sender As Object, e As EventArgs) Handles Me.Load
+        refreshgrid("")
+    End Sub
+
+    Private Sub addsiswabaru_ItemClick(sender As Object, e As DevExpress.XtraBars.ItemClickEventArgs) Handles addsiswabaru.ItemClick
+        Dim flagsave As Boolean = COA.initform(1, "")
+        If flagsave Then
+            refreshgrid("")
+        End If
+    End Sub
+
+    Private Sub edit_ItemClick(sender As Object, e As DevExpress.XtraBars.ItemClickEventArgs) Handles edit.ItemClick
+        Dim flagsave As Boolean
+
+        Dim keyid As String = GridView1.GetRowCellValue(GridView1.FocusedRowHandle, "keyid").ToString
+        flagsave = COA.initform(2, keyid)
+        If flagsave Then
+            refreshgrid("")
+        End If
+    End Sub
+
+    Private Sub deldata_ItemClick(sender As Object, e As DevExpress.XtraBars.ItemClickEventArgs) Handles deldata.ItemClick
+        Dim flagsave As Boolean
+
+        Dim keyid As String = GridView1.GetRowCellValue(GridView1.FocusedRowHandle, "keyid").ToString
+        flagsave = COA.initform(3, keyid)
+        If flagsave Then
+            refreshgrid("")
+        End If
+    End Sub
 End Class
