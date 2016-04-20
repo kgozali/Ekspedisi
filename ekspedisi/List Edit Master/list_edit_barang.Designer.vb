d2c965868a99a45ebe752ff725079d17d06cfbc4
Steve <setivess@gmail.com>
2016-04-21 05:13:23 +0700

    edit list barang

--- ekspedisi/List Edit Master/list_edit_barang.Designer.vb
+++ ekspedisi/List Edit Master/list_edit_barang.Designer.vb
@@ -22,19 +22,17 @@ Partial Class list_edit_barang
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(list_edit_barang))
         Me.GroupControl1 = New DevExpress.XtraEditors.GroupControl()
         Me.cancel = New DevExpress.XtraEditors.SimpleButton()
-        Me.Column4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.Column3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.hasiledit = New System.Windows.Forms.DataGridView()
+        Me.GridControl1 = New DevExpress.XtraGrid.GridControl()
+        Me.GridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
         CType(Me.GroupControl1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.GroupControl1.SuspendLayout()
-        CType(Me.hasiledit, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.GridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.GridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'GroupControl1
         '
-        Me.GroupControl1.Controls.Add(Me.hasiledit)
+        Me.GroupControl1.Controls.Add(Me.GridControl1)
         Me.GroupControl1.Location = New System.Drawing.Point(12, 12)
         Me.GroupControl1.Name = "GroupControl1"
         Me.GroupControl1.Size = New System.Drawing.Size(629, 302)
@@ -50,41 +48,19 @@ Partial Class list_edit_barang
         Me.cancel.TabIndex = 117
         Me.cancel.Text = "Cancel"
         '
-        'Column4
+        'GridControl1
         '
-        Me.Column4.HeaderText = "Keterangan"
-        Me.Column4.Name = "Column4"
-        Me.Column4.ReadOnly = True
-        Me.Column4.Width = 300
+        Me.GridControl1.Location = New System.Drawing.Point(5, 24)
+        Me.GridControl1.MainView = Me.GridView1
+        Me.GridControl1.Name = "GridControl1"
+        Me.GridControl1.Size = New System.Drawing.Size(619, 273)
+        Me.GridControl1.TabIndex = 0
+        Me.GridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.GridView1})
         '
-        'Column3
+        'GridView1
         '
-        Me.Column3.HeaderText = "Nama Principle"
-        Me.Column3.Name = "Column3"
-        Me.Column3.ReadOnly = True
-        '
-        'Column2
-        '
-        Me.Column2.HeaderText = "Nama Barang"
-        Me.Column2.Name = "Column2"
-        Me.Column2.ReadOnly = True
-        '
-        'Column1
-        '
-        Me.Column1.HeaderText = "ID Barang"
-        Me.Column1.Name = "Column1"
-        Me.Column1.ReadOnly = True
-        '
-        'hasiledit
-        '
-        Me.hasiledit.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.hasiledit.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2, Me.Column3, Me.Column4})
-        Me.hasiledit.Location = New System.Drawing.Point(5, 24)
-        Me.hasiledit.Name = "hasiledit"
-        Me.hasiledit.ReadOnly = True
-        Me.hasiledit.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
-        Me.hasiledit.Size = New System.Drawing.Size(618, 273)
-        Me.hasiledit.TabIndex = 48
+        Me.GridView1.GridControl = Me.GridControl1
+        Me.GridView1.Name = "GridView1"
         '
         'list_edit_barang
         '
@@ -97,15 +73,13 @@ Partial Class list_edit_barang
         Me.Text = "List Edit Barang"
         CType(Me.GroupControl1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.GroupControl1.ResumeLayout(False)
-        CType(Me.hasiledit, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.GridControl1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.GridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
     Friend WithEvents GroupControl1 As DevExpress.XtraEditors.GroupControl
-    Friend WithEvents hasiledit As System.Windows.Forms.DataGridView
-    Friend WithEvents Column1 As System.Windows.Forms.DataGridViewTextBoxColumn
-    Friend WithEvents Column2 As System.Windows.Forms.DataGridViewTextBoxColumn
-    Friend WithEvents Column3 As System.Windows.Forms.DataGridViewTextBoxColumn
-    Friend WithEvents Column4 As System.Windows.Forms.DataGridViewTextBoxColumn
     Friend WithEvents cancel As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents GridControl1 As DevExpress.XtraGrid.GridControl
+    Friend WithEvents GridView1 As DevExpress.XtraGrid.Views.Grid.GridView
 End Class
--- ekspedisi/List Edit Master/list_edit_barang.vb
+++ ekspedisi/List Edit Master/list_edit_barang.vb
@@ -1,3 +1,39 @@
-﻿Public Class list_edit_barang 
+﻿Imports MySql.Data.MySqlClient
+Public Class list_edit_barang
+    Dim data As New DataTable
+    Private Sub list_edit_barang_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
+            data.Columns.Add("Kode Barang")
+            data.Columns.Add("Nama Barang")
+            data.Columns.Add("Nama Principle")
+            data.Columns.Add("Keterangan")
 
+            For i = 0 To master_barang.GridView2.RowCount - 1
+                If master_barang.GridView2.IsRowSelected(i) Then
+                    Dim a As String = master_barang.GridView2.GetRowCellValue(i, "ID Barang").ToString()
+                    Dim b As String = master_barang.GridView2.GetRowCellValue(i, "Nama Barang").ToString()
+                    Dim c As String = master_barang.GridView2.GetRowCellValue(i, "Nama Principle").ToString()
+                    Dim d As String = master_barang.GridView2.GetRowCellValue(i, "Keterangan").ToString()
+                    data.Rows.Add(a, b, c, d)
+                End If
+            Next
+            GridControl1.DataSource = data
+            For i = 0 To data.Columns.Count - 1
+                GridView1.Columns(i).OptionsColumn.AllowEdit = False
+            Next
+
+        Catch ex As Exception
+
+        End Try
+
+    End Sub
+
+    Private Sub cancel_Click(sender As Object, e As EventArgs) Handles cancel.Click
+        Me.Close()
+    End Sub
+
+
+    Private Sub GridView1_RowCellClick(sender As Object, e As DevExpress.XtraGrid.Views.Grid.RowCellClickEventArgs) Handles GridView1.RowCellClick
+        edit_barang.ShowDialog()
+    End Sub
 End Class
