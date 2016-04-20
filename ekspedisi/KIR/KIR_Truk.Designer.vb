c76aba3866c46f6b9f81455120689d34c5be9093
Kevin Gozali <kgozali23@gmail.com>
2016-04-20 23:40:01 +0700

    hallo

--- ekspedisi/KIR/KIR_Truk.Designer.vb
+++ ekspedisi/KIR/KIR_Truk.Designer.vb
@@ -25,10 +25,8 @@ Partial Class KIR_Truk
         Me.GridControl1 = New DevExpress.XtraGrid.GridControl()
         Me.GridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
         Me.GroupControl1 = New DevExpress.XtraEditors.GroupControl()
-        Me.tanggalkir = New System.Windows.Forms.DateTimePicker()
-        Me.tanggalhabiskir = New System.Windows.Forms.RadioButton()
-        Me.nama = New System.Windows.Forms.RadioButton()
-        Me.cari = New DevExpress.XtraEditors.TextEdit()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
@@ -43,27 +41,31 @@ Partial Class KIR_Truk
         Me.Submit = New DevExpress.XtraEditors.SimpleButton()
         Me.GroupControl2 = New DevExpress.XtraEditors.GroupControl()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.TextEdit4 = New DevExpress.XtraEditors.TextEdit()
+        Me.SimpleButton2 = New DevExpress.XtraEditors.SimpleButton()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.ofd = New System.Windows.Forms.OpenFileDialog()
         CType(Me.GroupControl3, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.GroupControl3.SuspendLayout()
         CType(Me.GridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.GridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.GroupControl1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.GroupControl1.SuspendLayout()
-        CType(Me.cari.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.TextEdit1.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.TextEdit2.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.TextEdit3.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.GroupControl2, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.GroupControl2.SuspendLayout()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TextEdit4.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'GroupControl3
         '
         Me.GroupControl3.Controls.Add(Me.GridControl1)
-        Me.GroupControl3.Location = New System.Drawing.Point(271, 87)
+        Me.GroupControl3.Location = New System.Drawing.Point(12, 163)
         Me.GroupControl3.Name = "GroupControl3"
-        Me.GroupControl3.Size = New System.Drawing.Size(757, 388)
+        Me.GroupControl3.Size = New System.Drawing.Size(633, 469)
         Me.GroupControl3.TabIndex = 40
         Me.GroupControl3.Text = "Data KIR "
         '
@@ -74,7 +76,7 @@ Partial Class KIR_Truk
         Me.GridControl1.Location = New System.Drawing.Point(8, 24)
         Me.GridControl1.MainView = Me.GridView1
         Me.GridControl1.Name = "GridControl1"
-        Me.GridControl1.Size = New System.Drawing.Size(747, 359)
+        Me.GridControl1.Size = New System.Drawing.Size(623, 440)
         Me.GridControl1.TabIndex = 7
         Me.GridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.GridView1})
         '
@@ -85,52 +87,30 @@ Partial Class KIR_Truk
         '
         'GroupControl1
         '
-        Me.GroupControl1.Controls.Add(Me.tanggalkir)
-        Me.GroupControl1.Controls.Add(Me.tanggalhabiskir)
-        Me.GroupControl1.Controls.Add(Me.nama)
-        Me.GroupControl1.Controls.Add(Me.cari)
+        Me.GroupControl1.Controls.Add(Me.Label6)
+        Me.GroupControl1.Controls.Add(Me.ComboBox1)
         Me.GroupControl1.Location = New System.Drawing.Point(12, 87)
         Me.GroupControl1.Name = "GroupControl1"
-        Me.GroupControl1.Size = New System.Drawing.Size(251, 80)
+        Me.GroupControl1.Size = New System.Drawing.Size(251, 70)
         Me.GroupControl1.TabIndex = 39
-        Me.GroupControl1.Text = "Cari Berdasarkan"
+        Me.GroupControl1.Text = "Pilih Truk"
         '
-        'tanggalkir
+        'Label6
         '
-        Me.tanggalkir.Location = New System.Drawing.Point(5, 47)
-        Me.tanggalkir.Name = "tanggalkir"
-        Me.tanggalkir.Size = New System.Drawing.Size(200, 21)
-        Me.tanggalkir.TabIndex = 52
-        Me.tanggalkir.Visible = False
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(11, 36)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(50, 13)
+        Me.Label6.TabIndex = 1
+        Me.Label6.Text = "No. Polisi"
         '
-        'tanggalhabiskir
+        'ComboBox1
         '
-        Me.tanggalhabiskir.AutoSize = True
-        Me.tanggalhabiskir.Location = New System.Drawing.Point(93, 24)
-        Me.tanggalhabiskir.Name = "tanggalhabiskir"
-        Me.tanggalhabiskir.Size = New System.Drawing.Size(78, 17)
-        Me.tanggalhabiskir.TabIndex = 3
-        Me.tanggalhabiskir.Text = "Tanggal Kir"
-        Me.tanggalhabiskir.UseVisualStyleBackColor = True
-        '
-        'nama
-        '
-        Me.nama.AutoSize = True
-        Me.nama.Checked = True
-        Me.nama.Location = New System.Drawing.Point(5, 24)
-        Me.nama.Name = "nama"
-        Me.nama.Size = New System.Drawing.Size(82, 17)
-        Me.nama.TabIndex = 2
-        Me.nama.TabStop = True
-        Me.nama.Text = "Nomor Polisi"
-        Me.nama.UseVisualStyleBackColor = True
-        '
-        'cari
-        '
-        Me.cari.Location = New System.Drawing.Point(5, 50)
-        Me.cari.Name = "cari"
-        Me.cari.Size = New System.Drawing.Size(237, 20)
-        Me.cari.TabIndex = 0
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(67, 33)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(170, 21)
+        Me.ComboBox1.TabIndex = 0
         '
         'Label1
         '
@@ -156,7 +136,7 @@ Partial Class KIR_Truk
         '
         Me.Label3.AutoSize = True
         Me.Label3.Font = New System.Drawing.Font("Tahoma", 11.25!)
-        Me.Label3.Location = New System.Drawing.Point(734, 8)
+        Me.Label3.Location = New System.Drawing.Point(694, 8)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(89, 18)
         Me.Label3.TabIndex = 43
@@ -166,7 +146,7 @@ Partial Class KIR_Truk
         '
         Me.Label4.AutoSize = True
         Me.Label4.Font = New System.Drawing.Font("Tahoma", 11.25!)
-        Me.Label4.Location = New System.Drawing.Point(661, 35)
+        Me.Label4.Location = New System.Drawing.Point(621, 35)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(162, 18)
         Me.Label4.TabIndex = 44
@@ -176,7 +156,7 @@ Partial Class KIR_Truk
         '
         Me.Label5.AutoSize = True
         Me.Label5.Font = New System.Drawing.Font("Tahoma", 11.25!)
-        Me.Label5.Location = New System.Drawing.Point(710, 483)
+        Me.Label5.Location = New System.Drawing.Point(724, 62)
         Me.Label5.Name = "Label5"
         Me.Label5.Size = New System.Drawing.Size(59, 18)
         Me.Label5.TabIndex = 45
@@ -200,21 +180,21 @@ Partial Class KIR_Truk
         'TextEdit3
         '
         Me.TextEdit3.EditValue = "0"
-        Me.TextEdit3.Location = New System.Drawing.Point(775, 481)
+        Me.TextEdit3.Location = New System.Drawing.Point(789, 59)
         Me.TextEdit3.Name = "TextEdit3"
-        Me.TextEdit3.Size = New System.Drawing.Size(253, 20)
+        Me.TextEdit3.Size = New System.Drawing.Size(200, 20)
         Me.TextEdit3.TabIndex = 47
         '
         'DateTimePicker1
         '
-        Me.DateTimePicker1.Location = New System.Drawing.Point(829, 32)
+        Me.DateTimePicker1.Location = New System.Drawing.Point(789, 32)
         Me.DateTimePicker1.Name = "DateTimePicker1"
         Me.DateTimePicker1.Size = New System.Drawing.Size(200, 21)
         Me.DateTimePicker1.TabIndex = 48
         '
         'DateTimePicker2
         '
-        Me.DateTimePicker2.Location = New System.Drawing.Point(829, 6)
+        Me.DateTimePicker2.Location = New System.Drawing.Point(789, 6)
         Me.DateTimePicker2.Name = "DateTimePicker2"
         Me.DateTimePicker2.Size = New System.Drawing.Size(200, 21)
         Me.DateTimePicker2.TabIndex = 49
@@ -222,7 +202,7 @@ Partial Class KIR_Truk
         'SimpleButton1
         '
         Me.SimpleButton1.Image = CType(resources.GetObject("SimpleButton1.Image"), System.Drawing.Image)
-        Me.SimpleButton1.Location = New System.Drawing.Point(928, 514)
+        Me.SimpleButton1.Location = New System.Drawing.Point(896, 652)
         Me.SimpleButton1.Name = "SimpleButton1"
         Me.SimpleButton1.Size = New System.Drawing.Size(100, 27)
         Me.SimpleButton1.TabIndex = 51
@@ -231,7 +211,7 @@ Partial Class KIR_Truk
         'Submit
         '
         Me.Submit.Image = CType(resources.GetObject("Submit.Image"), System.Drawing.Image)
-        Me.Submit.Location = New System.Drawing.Point(822, 514)
+        Me.Submit.Location = New System.Drawing.Point(789, 652)
         Me.Submit.Name = "Submit"
         Me.Submit.Size = New System.Drawing.Size(100, 27)
         Me.Submit.TabIndex = 50
@@ -240,25 +220,57 @@ Partial Class KIR_Truk
         'GroupControl2
         '
         Me.GroupControl2.Controls.Add(Me.PictureBox1)
-        Me.GroupControl2.Location = New System.Drawing.Point(12, 173)
+        Me.GroupControl2.Location = New System.Drawing.Point(651, 163)
         Me.GroupControl2.Name = "GroupControl2"
-        Me.GroupControl2.Size = New System.Drawing.Size(251, 302)
+        Me.GroupControl2.Size = New System.Drawing.Size(338, 429)
         Me.GroupControl2.TabIndex = 52
         Me.GroupControl2.Text = "Bukti Kir"
         '
         'PictureBox1
         '
-        Me.PictureBox1.Location = New System.Drawing.Point(5, 24)
+        Me.PictureBox1.Location = New System.Drawing.Point(7, 24)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(241, 273)
+        Me.PictureBox1.Size = New System.Drawing.Size(328, 400)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.PictureBox1.TabIndex = 0
         Me.PictureBox1.TabStop = False
+        '
+        'TextEdit4
+        '
+        Me.TextEdit4.Enabled = False
+        Me.TextEdit4.Location = New System.Drawing.Point(684, 606)
+        Me.TextEdit4.Name = "TextEdit4"
+        Me.TextEdit4.Size = New System.Drawing.Size(257, 20)
+        Me.TextEdit4.TabIndex = 53
+        '
+        'SimpleButton2
+        '
+        Me.SimpleButton2.Image = CType(resources.GetObject("SimpleButton2.Image"), System.Drawing.Image)
+        Me.SimpleButton2.Location = New System.Drawing.Point(947, 597)
+        Me.SimpleButton2.Name = "SimpleButton2"
+        Me.SimpleButton2.Size = New System.Drawing.Size(42, 37)
+        Me.SimpleButton2.TabIndex = 54
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(651, 609)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(29, 13)
+        Me.Label7.TabIndex = 55
+        Me.Label7.Text = "Path"
+        '
+        'ofd
+        '
         '
         'KIR_Truk
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1038, 553)
+        Me.ClientSize = New System.Drawing.Size(1008, 691)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.SimpleButton2)
+        Me.Controls.Add(Me.TextEdit4)
         Me.Controls.Add(Me.GroupControl2)
         Me.Controls.Add(Me.SimpleButton1)
         Me.Controls.Add(Me.Submit)
@@ -284,13 +296,13 @@ Partial Class KIR_Truk
         CType(Me.GroupControl1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.GroupControl1.ResumeLayout(False)
         Me.GroupControl1.PerformLayout()
-        CType(Me.cari.Properties, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.TextEdit1.Properties, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.TextEdit2.Properties, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.TextEdit3.Properties, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.GroupControl2, System.ComponentModel.ISupportInitialize).EndInit()
         Me.GroupControl2.ResumeLayout(False)
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TextEdit4.Properties, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -299,8 +311,6 @@ Partial Class KIR_Truk
     Friend WithEvents GridControl1 As DevExpress.XtraGrid.GridControl
     Friend WithEvents GridView1 As DevExpress.XtraGrid.Views.Grid.GridView
     Friend WithEvents GroupControl1 As DevExpress.XtraEditors.GroupControl
-    Friend WithEvents nama As System.Windows.Forms.RadioButton
-    Friend WithEvents cari As DevExpress.XtraEditors.TextEdit
     Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents Label2 As System.Windows.Forms.Label
     Friend WithEvents Label3 As System.Windows.Forms.Label
@@ -313,8 +323,12 @@ Partial Class KIR_Truk
     Friend WithEvents DateTimePicker2 As System.Windows.Forms.DateTimePicker
     Friend WithEvents SimpleButton1 As DevExpress.XtraEditors.SimpleButton
     Friend WithEvents Submit As DevExpress.XtraEditors.SimpleButton
-    Friend WithEvents tanggalkir As System.Windows.Forms.DateTimePicker
-    Friend WithEvents tanggalhabiskir As System.Windows.Forms.RadioButton
     Friend WithEvents GroupControl2 As DevExpress.XtraEditors.GroupControl
     Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents TextEdit4 As DevExpress.XtraEditors.TextEdit
+    Friend WithEvents SimpleButton2 As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents ofd As System.Windows.Forms.OpenFileDialog
 End Class
--- ekspedisi/KIR/KIR_Truk.vb
+++ ekspedisi/KIR/KIR_Truk.vb
@@ -2,10 +2,20 @@
 Public Class KIR_Truk
 
     Dim kode As String = ""
+    Dim dt As New DataTable
+    Dim nominal As Integer = 0
+    Dim path As String = ""
+    Dim kodekir As String = ""
     Private Sub KIR_Truk_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Try
             'autogenerate kode kir
             autogen()
+            'select truk
+            dt = DtTable("select id_truk,no_pol from mtruk")
+            ComboBox1.DataSource = dt
+            ComboBox1.DisplayMember = "no_pol"
+            ComboBox1.ValueMember = "id_truk"
+            dt.Rows.Clear()
             'generate isi gridview
             cekcang()
         Catch ex As Exception
@@ -13,29 +23,19 @@ Public Class KIR_Truk
         End Try
     End Sub
 
-    Private Sub nama_CheckedChanged(sender As Object, e As EventArgs) Handles nama.CheckedChanged
+    Private Sub nama_CheckedChanged(sender As Object, e As EventArgs)
         cekcang()
     End Sub
 
     Sub cekcang()
         Try
-            If nama.Checked = True Then
-                tanggalkir.Visible = False
-                cari.Visible = True
-                Dim dt As New DataTable
-                dt = DtTable("select concat(day(kir.tgl),' ',monthname(kir.tgl),' ',year(kir.tgl)) `Tanggal KIR`,kir.id_kir `Kode KIR`,kir.no_kir `No. KIR`,mtruk.id_truk `Kode Truk`,mtruk.no_pol `No. Polisi` from kir,mtruk where kir.id_truk=mtruk.id_truk and mtruk.no_pol LIKE '%" + cari.Text + "%'")
-                GridControl1.DataSource = dt
-            ElseIf tanggalhabiskir.Checked = True Then
-                tanggalkir.Visible = True
-                cari.Visible = False
-                Dim dt As New DataTable
-                dt = DtTable("select concat(day(kir.tgl),' ',monthname(kir.tgl),' ',year(kir.tgl)) `Tanggal KIR`,kir.id_kir `Kode KIR`,kir.no_kir `No. KIR`,mtruk.id_truk `Kode Truk`,mtruk.no_pol `No. Polisi` from kir,mtruk where kir.id_truk=mtruk.id_truk and kir.tgl='" + tanggalkir.Value.Date.ToString("yyyy-MM-dd") + "'")
-                GridControl1.DataSource = dt
-            End If
+            dt = DtTable("select concat(day(kir.tgl),' ',monthname(kir.tgl),' ',year(kir.tgl)) `Tanggal KIR`,kir.id_kir `Kode KIR`,kir.no_kir `No. KIR`,mtruk.id_truk `Kode Truk`,mtruk.no_pol `No. Polisi` from kir,mtruk where kir.id_truk=mtruk.id_truk and mtruk.id_truk LIKE '%" + ComboBox1.SelectedValue.ToString + "%'")
+            GridControl1.DataSource = dt
+
         Catch ex As Exception
             MessageBox.Show(ex.Message, "System Warning", MessageBoxButtons.OK, MessageBoxIcon.Error)
         End Try
-        
+
     End Sub
 
     Sub autogen()
@@ -55,15 +55,15 @@ Public Class KIR_Truk
       
     End Sub
 
-    Private Sub tanggalhabiskir_CheckedChanged(sender As Object, e As EventArgs) Handles tanggalhabiskir.CheckedChanged
+    Private Sub tanggalhabiskir_CheckedChanged(sender As Object, e As EventArgs)
         cekcang()
     End Sub
 
-    Private Sub cari_EditValueChanged(sender As Object, e As EventArgs) Handles cari.EditValueChanged
+    Private Sub cari_EditValueChanged(sender As Object, e As EventArgs)
         cekcang()
     End Sub
 
-    Private Sub tanggalkir_ValueChanged(sender As Object, e As EventArgs) Handles tanggalkir.ValueChanged
+    Private Sub tanggalkir_ValueChanged(sender As Object, e As EventArgs)
         cekcang()
     End Sub
 
@@ -72,19 +72,107 @@ Public Class KIR_Truk
             If TextEdit2.Text = "" Then
                 MessageBox.Show("Nomor KIR wajib terisi", "System Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
             Else
-                If TextEdit3.Text = "0" Then
-                    Dim msg As Integer = MessageBox.Show("Nominal KIR tertulis Rp. 0, apakah anda ingin melanjutkan transaksi KIR? Tekan OK untuk konfirmasi", "System Warning", MessageBoxButtons.OKCancel, MessageBoxIcon.Exclamation)
-                    If msg = DialogResult.OK Then
-                        'insert database
+                If DateTimePicker1.Value < DateTimePicker2.Value Or DateTimePicker1.Value = DateTimePicker2.Value Then
+                    MessageBox.Show("Tanggal KIR berikutnya wajib lebih besar dari tanggal KIR saat ini", "System Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                Else
+                    If Not IsNumeric(TextEdit3.Text) Then
+                        MessageBox.Show("Nominal KIR wajib diisi dengan karakter numerik", "System Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                        TextEdit3.Text = ""
                     Else
+                        If TextEdit3.Text = "0.00" Or TextEdit3.Text = "" Or TextEdit3.Text = "0" Then
+                            Dim msg As Integer = MessageBox.Show("Nominal KIR Rp. 0 ,apakah anda ingin melanjutkan transaksi KIR? Tekan OK untuk konfirmasi", "System Warning", MessageBoxButtons.OKCancel, MessageBoxIcon.Exclamation)
+                            If msg = DialogResult.OK Then
+                                If TextEdit4.Text = "" Then
+                                    Dim msg1 As Integer = MessageBox.Show("Bukti KIR tidak ditemukan, apakah anda ingin melanjutkan tanpa menambahkan bukti KIR?", "System Warning", MessageBoxButtons.OKCancel, MessageBoxIcon.Exclamation)
+                                    If msg1 = DialogResult.OK Then
+                                        kodekir = TextEdit1.Text.ToString
+                                        Dim insert As Boolean = InsertInto("insert into kir values('" + kodekir + "','" + TextEdit2.Text.ToString + "',now(),'" + DateTimePicker2.Value.Date.ToString("yyyy-MM-dd") + "','" + ComboBox1.SelectedValue.ToString + "','" + DateTimePicker1.Value.Date.ToString("yyyy-MM-dd") + "','" + nominal.ToString + "','" + TextEdit4.Text.ToString + "')")
+                                        If insert = True Then
+                                            MessageBox.Show("KIR berhasil dilakukan", "System Error", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                                        End If
+                                        KIR_Truk_Load(sender, e)
+                                    End If
+                                Else
+                                    kodekir = TextEdit1.Text.ToString
+                                    Dim insert As Boolean = InsertInto("insert into kir values('" + kodekir + "','" + TextEdit2.Text.ToString + "',now(),'" + DateTimePicker2.Value.Date.ToString("yyyy-MM-dd") + "','" + ComboBox1.SelectedValue.ToString + "','" + DateTimePicker1.Value.Date.ToString("yyyy-MM-dd") + "','" + nominal.ToString + "','" + TextEdit4.Text.ToString + "')")
+                                    If insert = True Then
+                                        MessageBox.Show("KIR berhasil dilakukan", "System Error", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                                    End If
+                                    KIR_Truk_Load(sender, e)
+                                End If
 
+                            End If
+
+                        Else
+                            If TextEdit4.Text = "" Then
+                                Dim msg As Integer = MessageBox.Show("Bukti KIR tidak ditemukan, apakah anda ingin melanjutkan tanpa menambahkan bukti KIR?", "System Warning", MessageBoxButtons.OKCancel, MessageBoxIcon.Exclamation)
+                                If msg = DialogResult.OK Then
+                                    kodekir = TextEdit1.Text.ToString
+                                    Dim insert As Boolean = InsertInto("insert into kir values('" + kodekir + "','" + TextEdit2.Text.ToString + "',now(),'" + DateTimePicker2.Value.Date.ToString("yyyy-MM-dd") + "','" + ComboBox1.SelectedValue.ToString + "','" + DateTimePicker1.Value.Date.ToString("yyyy-MM-dd") + "','" + nominal.ToString + "','" + TextEdit4.Text.ToString + "')")
+                                    If insert = True Then
+                                        MessageBox.Show("KIR berhasil dilakukan", "System Error", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                                    End If
+                                    KIR_Truk_Load(sender, e)
+                                End If
+                            Else
+                                kodekir = TextEdit1.Text.ToString
+                                Dim insert As Boolean = InsertInto("insert into kir values('" + kodekir + "','" + TextEdit2.Text.ToString + "',now(),'" + DateTimePicker2.Value.Date.ToString("yyyy-MM-dd") + "','" + ComboBox1.SelectedValue.ToString + "','" + DateTimePicker1.Value.Date.ToString("yyyy-MM-dd") + "','" + nominal.ToString + "','" + TextEdit4.Text.ToString + "')")
+                                If insert = True Then
+                                    MessageBox.Show("KIR berhasil dilakukan", "System Error", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                                End If
+                                KIR_Truk_Load(sender, e)
+                            End If
+
+
+                        End If
                     End If
                 End If
+                
+
             End If
         Catch ex As Exception
             MessageBox.Show(ex.Message, "System Warning", MessageBoxButtons.OK, MessageBoxIcon.Error)
         End Try
        
 
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        cekcang()
+        GroupControl3.Text = "Data KIR "
+    End Sub
+
+    Private Sub TextEdit3_EditValueChanged(sender As Object, e As EventArgs) Handles TextEdit3.EditValueChanged
+
+        If TextEdit3.Text = "" Or Not IsNumeric(TextEdit3.Text) Then
+
+        Else
+            nominal = TextEdit3.Text
+            TextEdit3.Text = FormatNumber(Double.Parse(TextEdit3.Text).ToString(), 2)
+        End If
+
+    End Sub
+
+    Private Sub SimpleButton2_Click(sender As Object, e As EventArgs) Handles SimpleButton2.Click
+        ofd.FileName = ""
+        ofd.Filter = "JPG | *.jpg;*.jpeg|PNG | *.png"
+        ofd.DefaultExt = "PNG"
+        ofd.InitialDirectory = "C:\users\public"
+        ofd.ShowDialog()
+    End Sub
+
+    Private Sub ofd_FileOk(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles ofd.FileOk
+        TextEdit4.Text = ofd.FileName
+        path = ofd.FileName.Replace("\", "\\")
+    End Sub
+
+    Private Sub TextEdit4_EditValueChanged(sender As Object, e As EventArgs) Handles TextEdit4.EditValueChanged
+        PictureBox1.ImageLocation = TextEdit4.Text
+    End Sub
+
+    Private Sub GridControl1_DataSourceChanged(sender As Object, e As EventArgs) Handles GridControl1.DataSourceChanged
+        For i = 0 To GridView1.Columns.Count - 1
+            GridView1.Columns(i).OptionsColumn.AllowEdit = False
+        Next
     End Sub
 End Class
