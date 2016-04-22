401c77dad96706f6150df5c95c4edcd9bf8a236a
Steve <setivess@gmail.com>
2016-04-23 06:08:42 +0700

    tes

--- ekspedisi/Piutang Perusahaan/daftar_principle_pelunasan.vb
+++ ekspedisi/Piutang Perusahaan/daftar_principle_pelunasan.vb
@@ -1,3 +1,6 @@
 ﻿Public Class daftar_principle_pelunasan 
 
+    Private Sub daftar_principle_pelunasan_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
--- ekspedisi/Piutang Perusahaan/pelunasan_piutang.vb
+++ ekspedisi/Piutang Perusahaan/pelunasan_piutang.vb
@@ -1,4 +1,4 @@
-﻿Public Class pelunasan_piutang 
+﻿Public Class x
 
     Private Sub principle_ButtonPressed(sender As Object, e As DevExpress.XtraEditors.Controls.ButtonPressedEventArgs) Handles principle.ButtonPressed
         daftar_principle_pelunasan.ShowDialog()
