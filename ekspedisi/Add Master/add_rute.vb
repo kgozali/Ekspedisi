25e89dd4acc6ad13bf09cee422a1a8993f6bc5a1
Steve <setivess@gmail.com>
2016-04-24 17:26:50 +0700

    commit

--- ekspedisi/Add Master/add_rute.vb
+++ ekspedisi/Add Master/add_rute.vb
@@ -3,7 +3,9 @@ Public Class add_rute
     Dim cek As Boolean
     Dim cbasal As New DataTable
     Dim ctujuan As New DataTable
-    Private Sub cancel_Click(sender As Object, e As EventArgs) Handles cancel.Click
+    Dim cbprinciple As New DataTable
+
+    Private Sub cancel_Click(sender As Object, e As EventArgs)
         Me.Close()
     End Sub
 
@@ -26,16 +28,16 @@ Public Class add_rute
         End Try
     End Sub
 
-    Private Sub simpan_Click(sender As Object, e As EventArgs) Handles simpan.Click
+    Private Sub simpan_Click(sender As Object, e As EventArgs)
         If hargaunit.Text = "" Or unit.Text = "" Then
             MessageBox.Show("Mohon lengkapi data terlebih dahulu", "System Warning", MessageBoxButtons.OK, MessageBoxIcon.Error)
         Else
             Dim cc As New DataTable
-            cc = DtTable("select * from mrute where kota_asal = '" & ComboBox1.SelectedValue.ToString & "' and kota_tujuan = '" & ComboBox2.SelectedValue.ToString & "'")
+            cc = DtTable("select * from mrute where kota_asal = '" & asal.SelectedValue.ToString & "' and kota_tujuan = '" & tujuan.SelectedValue.ToString & "'")
             If cc.Rows.Count = 0 Then
                 Try
                     'insert ke dalam database
-                    InsertInto("insert into mrute values ('" & id.Text & "','" & ComboBox1.SelectedValue.ToString & "','" & ComboBox2.SelectedValue.ToString & "','" & hargaunit.Text & "','" & unit.Text & "') ")
+                    InsertInto("insert into mrute values ('" & id.Text & "','" & asal.SelectedValue.ToString & "','" & tujuan.SelectedValue.ToString & "','" & principle.SelectedValue.ToString & "','" & hargaunit.Text & "','" & unit.Text & "','1') ")
                     'konfirmasi melakukan booking ulang
                     Dim msg As Integer = MsgBox("Booking berhasil dilakukan, Apakah anda ingin melakukan input kembali?", MsgBoxStyle.YesNo, "System Message")
                     If msg = DialogResult.Yes Then
@@ -50,10 +52,10 @@ Public Class add_rute
                     MsgBox(ex.Message)
                 End Try
             Else
-                MessageBox.Show("Rute telah pernah dibuat", "System Warning", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                MessageBox.Show("Kota Asal dan Tujuan tidak boleh sama", "System Warning", MessageBoxButtons.OK, MessageBoxIcon.Error)
             End If
 
-            
+
         End If
 
 
@@ -63,7 +65,7 @@ Public Class add_rute
     Private Sub add_rute_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Dim tanggal As New DataTable
         Dim tgl As String = "MR"
-        tanggal = DtTable("select * from mrute where substring(ID_rute,1,10) = '" & tgl & "'")
+        tanggal = DtTable("select * from mrute where substring(ID_rute,1,2) = '" & tgl & "'")
         Dim hitung As String = tanggal.Rows.Count() + 1
         While hitung.LongCount < 5
             hitung = "0" + hitung
@@ -71,20 +73,25 @@ Public Class add_rute
         id.Text = tgl + hitung
 
         cbasal = DtTable("select kota from mkota")
-        ComboBox1.DataSource = cbasal
-        ComboBox1.DisplayMember = "Kota"
-        ComboBox1.ValueMember = "Kota"
+        asal.DataSource = cbasal
+        asal.DisplayMember = "Kota"
+        asal.ValueMember = "Kota"
         ctujuan = DtTable("select kota from mkota")
-        ComboBox2.DataSource = ctujuan
-        ComboBox2.DisplayMember = "Kota"
-        ComboBox2.ValueMember = "Kota"
+        tujuan.DataSource = ctujuan
+        tujuan.DisplayMember = "Kota"
+        tujuan.ValueMember = "Kota"
+
+        cbprinciple = DtTable("select id_principle `Kode Principle`,nama_principle `Nama Principle` from mprinciple")
+        principle.DataSource = cbprinciple
+        principle.DisplayMember = "Nama Principle"
+        principle.ValueMember = "Kode Principle"
         unit.Text = ""
         hargaunit.Text = ""
 
     End Sub
 
 
-    Private Sub hargaunit_KeyPress(sender As Object, e As KeyPressEventArgs) Handles hargaunit.KeyPress
+    Private Sub hargaunit_KeyPress(sender As Object, e As KeyPressEventArgs)
         If Not Char.IsNumber(e.KeyChar) AndAlso Not Char.IsControl(e.KeyChar) Then e.KeyChar = ""
         'pengecekan untuk mengetahui apakah form sudah di edit atau belum (jika belum, untuk menghindari system warning pertanyaan)
         If hargaunit.Text = "" Then
@@ -94,15 +101,5 @@ Public Class add_rute
         End If
     End Sub
 
-    Private Sub unit_KeyPress(sender As Object, e As KeyPressEventArgs) Handles unit.KeyPress
-        If Not Char.IsNumber(e.KeyChar) AndAlso Not Char.IsControl(e.KeyChar) Then e.KeyChar = ""
-        'pengecekan untuk mengetahui apakah form sudah di edit atau belum (jika belum, untuk menghindari system warning pertanyaan)
-        If unit.Text = "" Then
-            cek = False
-        Else
-            cek = True
-        End If
-    End Sub
 
- 
 End Class
