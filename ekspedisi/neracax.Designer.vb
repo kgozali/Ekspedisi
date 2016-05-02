7606a329abc69a0b9d1da87d5a3b26daed6bd121
Kevin Gozali - DELL <kevingozali@streetlabsoftware.com>
2016-05-02 14:46:35 +0700

    commit

--- ekspedisi/neracax.Designer.vb
+++ ekspedisi/neracax.Designer.vb
@@ -19,7 +19,10 @@ Partial Public Class neracax
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(neracax))
         Me.Detail = New DevExpress.XtraReports.UI.DetailBand()
+        Me.XrSubreport1 = New DevExpress.XtraReports.UI.XRSubreport()
+        Me.XrSubreport2 = New DevExpress.XtraReports.UI.XRSubreport()
         Me.TopMargin = New DevExpress.XtraReports.UI.TopMarginBand()
         Me.BottomMargin = New DevExpress.XtraReports.UI.BottomMarginBand()
         Me.PageFooter = New DevExpress.XtraReports.UI.PageFooterBand()
@@ -32,8 +35,11 @@ Partial Public Class neracax
         Me.XrLabel5 = New DevExpress.XtraReports.UI.XRLabel()
         Me.XrLine1 = New DevExpress.XtraReports.UI.XRLine()
         Me.XrLabel10 = New DevExpress.XtraReports.UI.XRLabel()
-        Me.XrSubreport1 = New DevExpress.XtraReports.UI.XRSubreport()
-        Me.XrSubreport2 = New DevExpress.XtraReports.UI.XRSubreport()
+        Me.XrLabel14 = New DevExpress.XtraReports.UI.XRLabel()
+        Me.XrLabel15 = New DevExpress.XtraReports.UI.XRLabel()
+        Me.XrLabel16 = New DevExpress.XtraReports.UI.XRLabel()
+        Me.XrLine2 = New DevExpress.XtraReports.UI.XRLine()
+        Me.XrPictureBox1 = New DevExpress.XtraReports.UI.XRPictureBox()
         CType(Me, System.ComponentModel.ISupportInitialize).BeginInit()
         '
         'Detail
@@ -45,6 +51,22 @@ Partial Public Class neracax
         Me.Detail.Name = "Detail"
         Me.Detail.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100.0!)
         Me.Detail.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+        '
+        'XrSubreport1
+        '
+        Me.XrSubreport1.CanShrink = True
+        Me.XrSubreport1.LocationFloat = New DevExpress.Utils.PointFloat(3.125!, 0.0!)
+        Me.XrSubreport1.Name = "XrSubreport1"
+        Me.XrSubreport1.ReportSource = New ekspedisi.subneraca_ak()
+        Me.XrSubreport1.SizeF = New System.Drawing.SizeF(395.8333!, 64.58334!)
+        '
+        'XrSubreport2
+        '
+        Me.XrSubreport2.CanShrink = True
+        Me.XrSubreport2.LocationFloat = New DevExpress.Utils.PointFloat(409.375!, 0.0!)
+        Me.XrSubreport2.Name = "XrSubreport2"
+        Me.XrSubreport2.ReportSource = New ekspedisi.subneraca_ak()
+        Me.XrSubreport2.SizeF = New System.Drawing.SizeF(389.5833!, 64.58334!)
         '
         'TopMargin
         '
@@ -89,58 +111,64 @@ Partial Public Class neracax
         '
         Me.XrCrossBandBox1.BorderWidth = 2.0!
         Me.XrCrossBandBox1.EndBand = Me.PageFooter
-        Me.XrCrossBandBox1.EndPointFloat = New DevExpress.Utils.PointFloat(3.125!, 2.000014!)
-        Me.XrCrossBandBox1.LocationFloat = New DevExpress.Utils.PointFloat(3.125!, 60.41667!)
+        Me.XrCrossBandBox1.EndPointFloat = New DevExpress.Utils.PointFloat(3.125!, 2.000046!)
+        Me.XrCrossBandBox1.LocationFloat = New DevExpress.Utils.PointFloat(3.125!, 222.9167!)
         Me.XrCrossBandBox1.Name = "XrCrossBandBox1"
         Me.XrCrossBandBox1.StartBand = Me.PageHeader
-        Me.XrCrossBandBox1.StartPointFloat = New DevExpress.Utils.PointFloat(3.125!, 60.41667!)
+        Me.XrCrossBandBox1.StartPointFloat = New DevExpress.Utils.PointFloat(3.125!, 222.9167!)
         Me.XrCrossBandBox1.WidthF = 817.7084!
         '
         'PageHeader
         '
-        Me.PageHeader.Controls.AddRange(New DevExpress.XtraReports.UI.XRControl() {Me.XrLabel8, Me.XrLabel6, Me.XrLabel5, Me.XrLine1, Me.XrLabel10})
-        Me.PageHeader.HeightF = 102.375!
+        Me.PageHeader.Controls.AddRange(New DevExpress.XtraReports.UI.XRControl() {Me.XrLabel14, Me.XrLabel15, Me.XrLabel16, Me.XrLine2, Me.XrPictureBox1, Me.XrLabel8, Me.XrLabel6, Me.XrLabel5, Me.XrLine1, Me.XrLabel10})
+        Me.PageHeader.HeightF = 268.0!
         Me.PageHeader.Name = "PageHeader"
         '
         'XrLabel8
         '
-        Me.XrLabel8.LocationFloat = New DevExpress.Utils.PointFloat(3.125!, 0.0!)
+        Me.XrLabel8.Font = New System.Drawing.Font("Arial", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.XrLabel8.LocationFloat = New DevExpress.Utils.PointFloat(5.208333!, 143.4167!)
         Me.XrLabel8.Name = "XrLabel8"
         Me.XrLabel8.Padding = New DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100.0!)
         Me.XrLabel8.SizeF = New System.Drawing.SizeF(810.875!, 33.12498!)
+        Me.XrLabel8.StylePriority.UseFont = False
         Me.XrLabel8.StylePriority.UseTextAlignment = False
         Me.XrLabel8.Text = "NERACA"
         Me.XrLabel8.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleCenter
         '
         'XrLabel6
         '
-        Me.XrLabel6.LocationFloat = New DevExpress.Utils.PointFloat(409.375!, 60.41667!)
+        Me.XrLabel6.Font = New System.Drawing.Font("Arial", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.XrLabel6.LocationFloat = New DevExpress.Utils.PointFloat(409.375!, 222.7917!)
         Me.XrLabel6.Name = "XrLabel6"
         Me.XrLabel6.Padding = New DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100.0!)
         Me.XrLabel6.SizeF = New System.Drawing.SizeF(389.5833!, 35.20832!)
+        Me.XrLabel6.StylePriority.UseFont = False
         Me.XrLabel6.StylePriority.UseTextAlignment = False
         Me.XrLabel6.Text = "KEWAJIBAN"
         Me.XrLabel6.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleCenter
         '
         'XrLabel5
         '
-        Me.XrLabel5.LocationFloat = New DevExpress.Utils.PointFloat(3.125!, 59.87501!)
+        Me.XrLabel5.Font = New System.Drawing.Font("Arial", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.XrLabel5.LocationFloat = New DevExpress.Utils.PointFloat(3.125032!, 222.25!)
         Me.XrLabel5.Name = "XrLabel5"
         Me.XrLabel5.Padding = New DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100.0!)
         Me.XrLabel5.SizeF = New System.Drawing.SizeF(395.8333!, 33.12499!)
+        Me.XrLabel5.StylePriority.UseFont = False
         Me.XrLabel5.StylePriority.UseTextAlignment = False
         Me.XrLabel5.Text = "AKTIVA"
         Me.XrLabel5.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleCenter
         '
         'XrLine1
         '
-        Me.XrLine1.LocationFloat = New DevExpress.Utils.PointFloat(1.041603!, 93.0!)
+        Me.XrLine1.LocationFloat = New DevExpress.Utils.PointFloat(1.041635!, 255.375!)
         Me.XrLine1.Name = "XrLine1"
         Me.XrLine1.SizeF = New System.Drawing.SizeF(817.7084!, 9.375!)
         '
         'XrLabel10
         '
-        Me.XrLabel10.LocationFloat = New DevExpress.Utils.PointFloat(0.0!, 33.12499!)
+        Me.XrLabel10.LocationFloat = New DevExpress.Utils.PointFloat(3.125!, 187.0417!)
         Me.XrLabel10.Name = "XrLabel10"
         Me.XrLabel10.Padding = New DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100.0!)
         Me.XrLabel10.SizeF = New System.Drawing.SizeF(814.0!, 14.37498!)
@@ -148,21 +176,55 @@ Partial Public Class neracax
         Me.XrLabel10.Text = "Bulan"
         Me.XrLabel10.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleCenter
         '
-        'XrSubreport1
+        'XrLabel14
         '
-        Me.XrSubreport1.CanShrink = True
-        Me.XrSubreport1.LocationFloat = New DevExpress.Utils.PointFloat(3.125!, 0.0!)
-        Me.XrSubreport1.Name = "XrSubreport1"
-        Me.XrSubreport1.ReportSource = New ekspedisi.subneraca_ak()
-        Me.XrSubreport1.SizeF = New System.Drawing.SizeF(395.8333!, 64.58334!)
+        Me.XrLabel14.BorderColor = System.Drawing.Color.Black
+        Me.XrLabel14.Font = New System.Drawing.Font("Arial", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.XrLabel14.LocationFloat = New DevExpress.Utils.PointFloat(193.6533!, 30.83334!)
+        Me.XrLabel14.Name = "XrLabel14"
+        Me.XrLabel14.Padding = New DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100.0!)
+        Me.XrLabel14.SizeF = New System.Drawing.SizeF(452.0833!, 24.04167!)
+        Me.XrLabel14.StylePriority.UseBorderColor = False
+        Me.XrLabel14.StylePriority.UseFont = False
+        Me.XrLabel14.StylePriority.UseTextAlignment = False
+        Me.XrLabel14.Text = "PT SENTONG SULTAN KERATON MOJOPAHIT"
+        Me.XrLabel14.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopCenter
         '
-        'XrSubreport2
+        'XrLabel15
         '
-        Me.XrSubreport2.CanShrink = True
-        Me.XrSubreport2.LocationFloat = New DevExpress.Utils.PointFloat(409.375!, 0.0!)
-        Me.XrSubreport2.Name = "XrSubreport2"
-        Me.XrSubreport2.ReportSource = New ekspedisi.subneraca_ak()
-        Me.XrSubreport2.SizeF = New System.Drawing.SizeF(389.5833!, 64.58334!)
+        Me.XrLabel15.Font = New System.Drawing.Font("Arial Narrow", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.XrLabel15.LocationFloat = New DevExpress.Utils.PointFloat(277.2953!, 54.87502!)
+        Me.XrLabel15.Name = "XrLabel15"
+        Me.XrLabel15.Padding = New DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100.0!)
+        Me.XrLabel15.SizeF = New System.Drawing.SizeF(296.0518!, 23.0!)
+        Me.XrLabel15.StylePriority.UseFont = False
+        Me.XrLabel15.Text = "DUSUN JABON DS. JABON RT 01 RW 01"
+        '
+        'XrLabel16
+        '
+        Me.XrLabel16.Font = New System.Drawing.Font("Arial Narrow", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.XrLabel16.LocationFloat = New DevExpress.Utils.PointFloat(350.7081!, 77.87501!)
+        Me.XrLabel16.Name = "XrLabel16"
+        Me.XrLabel16.Padding = New DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100.0!)
+        Me.XrLabel16.SizeF = New System.Drawing.SizeF(122.608!, 16.75!)
+        Me.XrLabel16.StylePriority.UseFont = False
+        Me.XrLabel16.StylePriority.UseTextAlignment = False
+        Me.XrLabel16.Text = "JOMBANG"
+        Me.XrLabel16.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleCenter
+        '
+        'XrLine2
+        '
+        Me.XrLine2.LocationFloat = New DevExpress.Utils.PointFloat(10.00001!, 130.8334!)
+        Me.XrLine2.Name = "XrLine2"
+        Me.XrLine2.SizeF = New System.Drawing.SizeF(804.0002!, 12.5833!)
+        '
+        'XrPictureBox1
+        '
+        Me.XrPictureBox1.Image = CType(resources.GetObject("XrPictureBox1.Image"), System.Drawing.Image)
+        Me.XrPictureBox1.LocationFloat = New DevExpress.Utils.PointFloat(50.45303!, 30.83334!)
+        Me.XrPictureBox1.Name = "XrPictureBox1"
+        Me.XrPictureBox1.SizeF = New System.Drawing.SizeF(102.0833!, 100.0!)
+        Me.XrPictureBox1.Sizing = DevExpress.XtraPrinting.ImageSizeMode.Squeeze
         '
         'neracax
         '
@@ -189,4 +251,9 @@ Partial Public Class neracax
     Friend WithEvents XrLabel10 As DevExpress.XtraReports.UI.XRLabel
     Friend WithEvents XrSubreport2 As DevExpress.XtraReports.UI.XRSubreport
     Friend WithEvents XrSubreport1 As DevExpress.XtraReports.UI.XRSubreport
+    Friend WithEvents XrLabel14 As DevExpress.XtraReports.UI.XRLabel
+    Friend WithEvents XrLabel15 As DevExpress.XtraReports.UI.XRLabel
+    Friend WithEvents XrLabel16 As DevExpress.XtraReports.UI.XRLabel
+    Friend WithEvents XrLine2 As DevExpress.XtraReports.UI.XRLine
+    Friend WithEvents XrPictureBox1 As DevExpress.XtraReports.UI.XRPictureBox
 End Class
