8727002b126e61db15bb4d3e93199c0662fa91cb
Kevin Gozali - DELL <kevingozali@streetlabsoftware.com>
2016-05-03 23:34:58 +0700

    commit login

---
+++ ekspedisi/login2.Designer.vb
@@ -0,0 +1,112 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class login2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(login2))
+        Me.login = New DevExpress.XtraEditors.SimpleButton()
+        Me.cancel = New DevExpress.XtraEditors.SimpleButton()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.username = New System.Windows.Forms.TextBox()
+        Me.password = New System.Windows.Forms.MaskedTextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'login
+        '
+        Me.login.Image = CType(resources.GetObject("login.Image"), System.Drawing.Image)
+        Me.login.Location = New System.Drawing.Point(64, 102)
+        Me.login.Name = "login"
+        Me.login.Size = New System.Drawing.Size(100, 27)
+        Me.login.TabIndex = 170
+        Me.login.Text = "Login"
+        '
+        'cancel
+        '
+        Me.cancel.Image = CType(resources.GetObject("cancel.Image"), System.Drawing.Image)
+        Me.cancel.Location = New System.Drawing.Point(211, 102)
+        Me.cancel.Name = "cancel"
+        Me.cancel.Size = New System.Drawing.Size(100, 27)
+        Me.cancel.TabIndex = 169
+        Me.cancel.Text = "Cancel"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(61, 32)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(75, 16)
+        Me.Label1.TabIndex = 171
+        Me.Label1.Text = "Username :"
+        '
+        'username
+        '
+        Me.username.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.username.Location = New System.Drawing.Point(150, 29)
+        Me.username.Name = "username"
+        Me.username.Size = New System.Drawing.Size(161, 22)
+        Me.username.TabIndex = 172
+        '
+        'password
+        '
+        Me.password.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.password.Location = New System.Drawing.Point(150, 55)
+        Me.password.Name = "password"
+        Me.password.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.password.Size = New System.Drawing.Size(161, 22)
+        Me.password.TabIndex = 173
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(61, 58)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(73, 16)
+        Me.Label2.TabIndex = 174
+        Me.Label2.Text = "Password :"
+        '
+        'login2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(374, 151)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.password)
+        Me.Controls.Add(Me.username)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.login)
+        Me.Controls.Add(Me.cancel)
+        Me.Name = "login2"
+        Me.Text = "Login"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents login As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents cancel As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents username As System.Windows.Forms.TextBox
+    Friend WithEvents password As System.Windows.Forms.MaskedTextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+End Class
---
+++ ekspedisi/login2.vb
@@ -0,0 +1,48 @@
+﻿Public Class login2
+    Dim cek As Boolean = False
+    Private Sub login2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        password.Text = ""
+        username.Text = ""
+    End Sub
+
+    Private Sub cancel_Click(sender As Object, e As EventArgs) Handles cancel.Click
+        Me.Close()
+        main_menu.quit(sender, e)
+    End Sub
+    Private Sub formclosing(sender As Object, e As EventArgs) Handles MyBase.FormClosing
+        If cek = False Then
+            main_menu.quit(sender, e)
+        End If
+    End Sub
+    Private Sub login_Click(sender As Object, e As EventArgs) Handles login.Click
+        Try
+            Dim check As String = "select username from muser where username = '" & username.Text & "' "
+            Dim simpan As New DataTable
+            simpan = DtTable(check)
+            If simpan.Rows.Count = 0 Then
+                MessageBox.Show("Username Salah", "System Information", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                username.Text = ""
+            Else
+                Dim check2 As String = "select username, password from muser where username = '" & username.Text & "' and password = md5('" & password.Text & "') "
+                Dim simpan2 As New DataTable
+                simpan2 = DtTable(check2)
+                If simpan2.Rows.Count = 0 Then
+                    MessageBox.Show("Password Salah", "System Information", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+                Else
+                    Me.Close()
+                End If
+            End If
+        Catch ex As Exception
+
+        End Try
+
+    End Sub
+    Sub checkakses()
+        Dim selectall As String = "select * from hak_akses"
+        Dim tableselect As New DataTable
+        tableselect = DtTable(selectall)
+
+    End Sub
+
+End Class
