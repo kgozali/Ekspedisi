77c3e20241e625631b9a467cab1b68f3d91992a9
Kevin-Alienware15R2 <kgozali23@gmail.com>
2016-06-26 21:14:02 +0700

    hellowww

---
+++ ekspedisi/Fitur tambahan/Transaksi_sewa_mobil.Designer.vb
@@ -0,0 +1,213 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Transaksi_sewa_mobil
+    Inherits DevExpress.XtraEditors.XtraForm
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        If disposing AndAlso components IsNot Nothing Then
+            components.Dispose()
+        End If
+        MyBase.Dispose(disposing)
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Transaksi_sewa_mobil))
+        Me.RibbonControl1 = New DevExpress.XtraBars.Ribbon.RibbonControl()
+        Me.RibbonGalleryBarItem1 = New DevExpress.XtraBars.RibbonGalleryBarItem()
+        Me.datasiswa = New DevExpress.XtraBars.BarButtonItem()
+        Me.BarSubItem1 = New DevExpress.XtraBars.BarSubItem()
+        Me.BarButtonItem3 = New DevExpress.XtraBars.BarButtonItem()
+        Me.BarButtonItem5 = New DevExpress.XtraBars.BarButtonItem()
+        Me.mastersiswa = New DevExpress.XtraBars.BarButtonItem()
+        Me.BarButtonItem4 = New DevExpress.XtraBars.BarButtonItem()
+        Me.BarButtonItem6 = New DevExpress.XtraBars.BarButtonItem()
+        Me.BarButtonItem7 = New DevExpress.XtraBars.BarButtonItem()
+        Me.BarButtonItem8 = New DevExpress.XtraBars.BarButtonItem()
+        Me.BarButtonItem9 = New DevExpress.XtraBars.BarButtonItem()
+        Me.addsiswabaru = New DevExpress.XtraBars.BarButtonItem()
+        Me.edit = New DevExpress.XtraBars.BarButtonItem()
+        Me.deldata = New DevExpress.XtraBars.BarButtonItem()
+        Me.jumpmenu = New DevExpress.XtraBars.BarButtonItem()
+        Me.RibbonPage1 = New DevExpress.XtraBars.Ribbon.RibbonPage()
+        Me.RibbonPageGroup1 = New DevExpress.XtraBars.Ribbon.RibbonPageGroup()
+        CType(Me.RibbonControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'RibbonControl1
+        '
+        Me.RibbonControl1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.RibbonControl1.Dock = System.Windows.Forms.DockStyle.None
+        Me.RibbonControl1.ExpandCollapseItem.Id = 0
+        Me.RibbonControl1.Items.AddRange(New DevExpress.XtraBars.BarItem() {Me.RibbonControl1.ExpandCollapseItem, Me.RibbonGalleryBarItem1, Me.datasiswa, Me.BarSubItem1, Me.mastersiswa, Me.BarButtonItem3, Me.BarButtonItem4, Me.BarButtonItem5, Me.BarButtonItem6, Me.BarButtonItem7, Me.BarButtonItem8, Me.BarButtonItem9, Me.addsiswabaru, Me.edit, Me.deldata, Me.jumpmenu})
+        Me.RibbonControl1.Location = New System.Drawing.Point(0, -1)
+        Me.RibbonControl1.MaxItemId = 32
+        Me.RibbonControl1.Name = "RibbonControl1"
+        Me.RibbonControl1.Pages.AddRange(New DevExpress.XtraBars.Ribbon.RibbonPage() {Me.RibbonPage1})
+        Me.RibbonControl1.ShowApplicationButton = DevExpress.Utils.DefaultBoolean.[False]
+        Me.RibbonControl1.ShowExpandCollapseButton = DevExpress.Utils.DefaultBoolean.[False]
+        Me.RibbonControl1.ShowFullScreenButton = DevExpress.Utils.DefaultBoolean.[True]
+        Me.RibbonControl1.ShowPageHeadersMode = DevExpress.XtraBars.Ribbon.ShowPageHeadersMode.Hide
+        Me.RibbonControl1.ShowToolbarCustomizeItem = False
+        Me.RibbonControl1.Size = New System.Drawing.Size(1010, 96)
+        Me.RibbonControl1.Toolbar.ShowCustomizeItem = False
+        Me.RibbonControl1.ToolbarLocation = DevExpress.XtraBars.Ribbon.RibbonQuickAccessToolbarLocation.Hidden
+        '
+        'RibbonGalleryBarItem1
+        '
+        Me.RibbonGalleryBarItem1.Caption = "InplaceGallery1"
+        Me.RibbonGalleryBarItem1.Id = 11
+        Me.RibbonGalleryBarItem1.Name = "RibbonGalleryBarItem1"
+        '
+        'datasiswa
+        '
+        Me.datasiswa.Caption = "Master Data Siswa"
+        Me.datasiswa.Glyph = CType(resources.GetObject("datasiswa.Glyph"), System.Drawing.Image)
+        Me.datasiswa.Id = 10
+        Me.datasiswa.Name = "datasiswa"
+        '
+        'BarSubItem1
+        '
+        Me.BarSubItem1.Caption = "BarSubItem1"
+        Me.BarSubItem1.Id = 12
+        Me.BarSubItem1.LinksPersistInfo.AddRange(New DevExpress.XtraBars.LinkPersistInfo() {New DevExpress.XtraBars.LinkPersistInfo(Me.BarButtonItem3), New DevExpress.XtraBars.LinkPersistInfo(Me.BarButtonItem5)})
+        Me.BarSubItem1.Name = "BarSubItem1"
+        '
+        'BarButtonItem3
+        '
+        Me.BarButtonItem3.Caption = "Master Data Siswa"
+        Me.BarButtonItem3.Id = 15
+        Me.BarButtonItem3.Name = "BarButtonItem3"
+        '
+        'BarButtonItem5
+        '
+        Me.BarButtonItem5.Caption = "Master Data Level"
+        Me.BarButtonItem5.Id = 17
+        Me.BarButtonItem5.Name = "BarButtonItem5"
+        '
+        'mastersiswa
+        '
+        Me.mastersiswa.Caption = "Master Data Siswa"
+        Me.mastersiswa.Id = 13
+        Me.mastersiswa.LargeGlyph = CType(resources.GetObject("mastersiswa.LargeGlyph"), System.Drawing.Image)
+        Me.mastersiswa.LargeGlyphDisabled = CType(resources.GetObject("mastersiswa.LargeGlyphDisabled"), System.Drawing.Image)
+        Me.mastersiswa.Name = "mastersiswa"
+        '
+        'BarButtonItem4
+        '
+        Me.BarButtonItem4.Caption = "Master Data"
+        Me.BarButtonItem4.Id = 16
+        Me.BarButtonItem4.Name = "BarButtonItem4"
+        '
+        'BarButtonItem6
+        '
+        Me.BarButtonItem6.Caption = "Master Data Buku"
+        Me.BarButtonItem6.Id = 19
+        Me.BarButtonItem6.Name = "BarButtonItem6"
+        '
+        'BarButtonItem7
+        '
+        Me.BarButtonItem7.Caption = "Master Data Ruang"
+        Me.BarButtonItem7.Id = 20
+        Me.BarButtonItem7.Name = "BarButtonItem7"
+        '
+        'BarButtonItem8
+        '
+        Me.BarButtonItem8.Caption = "Master Data Guru"
+        Me.BarButtonItem8.Id = 21
+        Me.BarButtonItem8.Name = "BarButtonItem8"
+        '
+        'BarButtonItem9
+        '
+        Me.BarButtonItem9.Caption = "Master Data User"
+        Me.BarButtonItem9.Id = 22
+        Me.BarButtonItem9.Name = "BarButtonItem9"
+        '
+        'addsiswabaru
+        '
+        Me.addsiswabaru.Caption = "Tambah Data"
+        Me.addsiswabaru.Glyph = CType(resources.GetObject("addsiswabaru.Glyph"), System.Drawing.Image)
+        Me.addsiswabaru.Id = 23
+        Me.addsiswabaru.LargeGlyph = CType(resources.GetObject("addsiswabaru.LargeGlyph"), System.Drawing.Image)
+        Me.addsiswabaru.Name = "addsiswabaru"
+        '
+        'edit
+        '
+        Me.edit.Caption = "Edit Data"
+        Me.edit.Glyph = CType(resources.GetObject("edit.Glyph"), System.Drawing.Image)
+        Me.edit.Id = 27
+        Me.edit.LargeGlyph = CType(resources.GetObject("edit.LargeGlyph"), System.Drawing.Image)
+        Me.edit.Name = "edit"
+        '
+        'deldata
+        '
+        Me.deldata.ButtonStyle = DevExpress.XtraBars.BarButtonStyle.Check
+        Me.deldata.Caption = "Delete Data"
+        Me.deldata.Glyph = CType(resources.GetObject("deldata.Glyph"), System.Drawing.Image)
+        Me.deldata.Id = 30
+        Me.deldata.LargeGlyph = CType(resources.GetObject("deldata.LargeGlyph"), System.Drawing.Image)
+        Me.deldata.Name = "deldata"
+        '
+        'jumpmenu
+        '
+        Me.jumpmenu.Caption = "Main Menu"
+        Me.jumpmenu.Glyph = CType(resources.GetObject("jumpmenu.Glyph"), System.Drawing.Image)
+        Me.jumpmenu.Id = 31
+        Me.jumpmenu.LargeGlyph = CType(resources.GetObject("jumpmenu.LargeGlyph"), System.Drawing.Image)
+        Me.jumpmenu.Name = "jumpmenu"
+        '
+        'RibbonPage1
+        '
+        Me.RibbonPage1.Groups.AddRange(New DevExpress.XtraBars.Ribbon.RibbonPageGroup() {Me.RibbonPageGroup1})
+        Me.RibbonPage1.Name = "RibbonPage1"
+        Me.RibbonPage1.Text = "RibbonPage1"
+        '
+        'RibbonPageGroup1
+        '
+        Me.RibbonPageGroup1.ItemLinks.Add(Me.jumpmenu)
+        Me.RibbonPageGroup1.ItemLinks.Add(Me.addsiswabaru)
+        Me.RibbonPageGroup1.ItemLinks.Add(Me.edit)
+        Me.RibbonPageGroup1.ItemLinks.Add(Me.deldata)
+        Me.RibbonPageGroup1.Name = "RibbonPageGroup1"
+        '
+        'Transaksi_sewa_mobil
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1008, 691)
+        Me.Controls.Add(Me.RibbonControl1)
+        Me.Name = "Transaksi_sewa_mobil"
+        Me.Text = "Transaksi Sewa Mobil"
+        CType(Me.RibbonControl1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents RibbonControl1 As DevExpress.XtraBars.Ribbon.RibbonControl
+    Friend WithEvents RibbonGalleryBarItem1 As DevExpress.XtraBars.RibbonGalleryBarItem
+    Friend WithEvents datasiswa As DevExpress.XtraBars.BarButtonItem
+    Friend WithEvents BarSubItem1 As DevExpress.XtraBars.BarSubItem
+    Friend WithEvents BarButtonItem3 As DevExpress.XtraBars.BarButtonItem
+    Friend WithEvents BarButtonItem5 As DevExpress.XtraBars.BarButtonItem
+    Friend WithEvents mastersiswa As DevExpress.XtraBars.BarButtonItem
+    Friend WithEvents BarButtonItem4 As DevExpress.XtraBars.BarButtonItem
+    Friend WithEvents BarButtonItem6 As DevExpress.XtraBars.BarButtonItem
+    Friend WithEvents BarButtonItem7 As DevExpress.XtraBars.BarButtonItem
+    Friend WithEvents BarButtonItem8 As DevExpress.XtraBars.BarButtonItem
+    Friend WithEvents BarButtonItem9 As DevExpress.XtraBars.BarButtonItem
+    Friend WithEvents addsiswabaru As DevExpress.XtraBars.BarButtonItem
+    Friend WithEvents edit As DevExpress.XtraBars.BarButtonItem
+    Friend WithEvents deldata As DevExpress.XtraBars.BarButtonItem
+    Friend WithEvents jumpmenu As DevExpress.XtraBars.BarButtonItem
+    Friend WithEvents RibbonPage1 As DevExpress.XtraBars.Ribbon.RibbonPage
+    Friend WithEvents RibbonPageGroup1 As DevExpress.XtraBars.Ribbon.RibbonPageGroup
+End Class
---
+++ ekspedisi/Fitur tambahan/Transaksi_sewa_mobil.vb
@@ -0,0 +1,3 @@
+﻿Public Class Transaksi_sewa_mobil 
+
+End Class
