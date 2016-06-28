00109eaf85a4e24f02c4a8f815071faa767d63c0
Kevin-Alienware15R2 <kgozali23@gmail.com>
2016-06-28 08:43:08 +0700

    edit fix

--- ekspedisi/Add Master/add_mobil_sewa.vb
+++ ekspedisi/Add Master/add_mobil_sewa.vb
@@ -15,6 +15,15 @@
     End Sub
 
     Private Sub simpanedit_Click(sender As Object, e As EventArgs) Handles simpanedit.Click
-
+        Try
+            InsertInto("update mmobil set no_pol='" & nomorpolisi.Text & "',tgl_beli=" & tanggal.Value.ToString("yyyyMMdd") & ",tipe_mobil='" & tipemobil.Text & "',warna='" & warna.Text & "',tahun='" & tahun.Text & "' where id_mobil='" & id.Text & "'")
+            With edit_list_mobil.GridView1
+                .SetRowCellValue(.FocusedRowHandle, "No Polisi", nomorpolisi.Text)
+                .SetRowCellValue(.FocusedRowHandle, "Tanggal Beli", tanggal.Value.ToString("dd-MMMM-yyyy"))
+            End With
+            MessageBox.Show("Data berhasil diupdate", "Update Confirmation", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "System Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
     End Sub
 End Class
