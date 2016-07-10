4277f68ed1a89dc9ddeecee30da24fa5e301ac01
Kevin-Alienware15R2 <kgozali23@gmail.com>
2016-07-10 21:41:52 +0700

    coding daftar mobil

--- ekspedisi/Fitur tambahan/add_mobil_master.vb
+++ ekspedisi/Fitur tambahan/add_mobil_master.vb
@@ -1,3 +1,27 @@
 ﻿Public Class add_mobil_master 
 
+    Private Sub add_mobil_master_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        cari_EditValueChanged(sender, e)
+    End Sub
+
+    Dim data As New DataTable
+    Private Sub cari_EditValueChanged(sender As Object, e As EventArgs) Handles cari.EditValueChanged
+        Try
+            If nama.Checked = True Then
+                GridControl1.DataSource = DtTable("SELECT id_mobil `Kode Mobil`, no_pol `Nomor Polisi`, tipe_mobil `Tipe Mobil`,Warna,Tahun FROM mmobil m where s='1' and no_pol like '%" & cari.Text & "%'")
+            Else
+                GridControl1.DataSource = DtTable("SELECT id_mobil `Kode Mobil`, no_pol `Nomor Polisi`, tipe_mobil `Tipe Mobil`,Warna,Tahun FROM mmobil m where s='1' and id_mobil like '%" & cari.Text & "%'")
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "System Warning", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub SimpleButton1_Click(sender As Object, e As EventArgs) Handles cancel.Click
+        Me.Close()
+    End Sub
+
+    Private Sub nama_CheckedChanged(sender As Object, e As EventArgs) Handles nama.CheckedChanged
+        cari_EditValueChanged(sender, e)
+    End Sub
 End Class
