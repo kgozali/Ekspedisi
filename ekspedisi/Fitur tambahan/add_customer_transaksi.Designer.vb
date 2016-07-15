8d055f52d2cb3d441e209cc7850b07158bc4dcf6
Kevin-Alienware15R2 <kgozali23@gmail.com>
2016-07-15 20:35:50 +0700

    form baru

---
+++ ekspedisi/Fitur tambahan/add_customer_transaksi.Designer.vb
@@ -0,0 +1,168 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class add_customer_transaksi
+    Inherits DevExpress.XtraEditors.XtraForm
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        If disposing AndAlso components IsNot Nothing Then
+            components.Dispose()
+        End If
+        MyBase.Dispose(disposing)
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(add_customer_transaksi))
+        Me.GroupControl3 = New DevExpress.XtraEditors.GroupControl()
+        Me.GridControl1 = New DevExpress.XtraGrid.GridControl()
+        Me.GridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
+        Me.dataview = New System.Windows.Forms.DataGridView()
+        Me.submit = New DevExpress.XtraEditors.SimpleButton()
+        Me.GroupControl1 = New DevExpress.XtraEditors.GroupControl()
+        Me.nama = New System.Windows.Forms.RadioButton()
+        Me.id = New System.Windows.Forms.RadioButton()
+        Me.cari = New DevExpress.XtraEditors.TextEdit()
+        Me.cancel = New DevExpress.XtraEditors.SimpleButton()
+        CType(Me.GroupControl3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupControl3.SuspendLayout()
+        CType(Me.GridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.GridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.dataview, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.GroupControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupControl1.SuspendLayout()
+        CType(Me.cari.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GroupControl3
+        '
+        Me.GroupControl3.Controls.Add(Me.GridControl1)
+        Me.GroupControl3.Controls.Add(Me.dataview)
+        Me.GroupControl3.Location = New System.Drawing.Point(12, 94)
+        Me.GroupControl3.Name = "GroupControl3"
+        Me.GroupControl3.Size = New System.Drawing.Size(760, 422)
+        Me.GroupControl3.TabIndex = 48
+        Me.GroupControl3.Text = "Data Customer"
+        '
+        'GridControl1
+        '
+        Me.GridControl1.Location = New System.Drawing.Point(5, 24)
+        Me.GridControl1.MainView = Me.GridView1
+        Me.GridControl1.Name = "GridControl1"
+        Me.GridControl1.Size = New System.Drawing.Size(750, 389)
+        Me.GridControl1.TabIndex = 7
+        Me.GridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.GridView1})
+        '
+        'GridView1
+        '
+        Me.GridView1.GridControl = Me.GridControl1
+        Me.GridView1.Name = "GridView1"
+        '
+        'dataview
+        '
+        Me.dataview.AllowUserToAddRows = False
+        Me.dataview.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dataview.Location = New System.Drawing.Point(5, 24)
+        Me.dataview.Name = "dataview"
+        Me.dataview.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.dataview.Size = New System.Drawing.Size(672, 311)
+        Me.dataview.TabIndex = 6
+        '
+        'submit
+        '
+        Me.submit.Image = CType(resources.GetObject("submit.Image"), System.Drawing.Image)
+        Me.submit.Location = New System.Drawing.Point(566, 522)
+        Me.submit.Name = "submit"
+        Me.submit.Size = New System.Drawing.Size(100, 27)
+        Me.submit.TabIndex = 49
+        Me.submit.Text = "Submit"
+        '
+        'GroupControl1
+        '
+        Me.GroupControl1.Controls.Add(Me.nama)
+        Me.GroupControl1.Controls.Add(Me.id)
+        Me.GroupControl1.Controls.Add(Me.cari)
+        Me.GroupControl1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupControl1.Name = "GroupControl1"
+        Me.GroupControl1.Size = New System.Drawing.Size(263, 76)
+        Me.GroupControl1.TabIndex = 47
+        Me.GroupControl1.Text = "Cari Berdasarkan"
+        '
+        'nama
+        '
+        Me.nama.AutoSize = True
+        Me.nama.Checked = True
+        Me.nama.Location = New System.Drawing.Point(109, 24)
+        Me.nama.Name = "nama"
+        Me.nama.Size = New System.Drawing.Size(101, 17)
+        Me.nama.TabIndex = 2
+        Me.nama.TabStop = True
+        Me.nama.Text = "Nama Customer"
+        Me.nama.UseVisualStyleBackColor = True
+        '
+        'id
+        '
+        Me.id.AutoSize = True
+        Me.id.Location = New System.Drawing.Point(5, 24)
+        Me.id.Name = "id"
+        Me.id.Size = New System.Drawing.Size(98, 17)
+        Me.id.TabIndex = 1
+        Me.id.Text = "Kode Customer"
+        Me.id.UseVisualStyleBackColor = True
+        '
+        'cari
+        '
+        Me.cari.Location = New System.Drawing.Point(5, 51)
+        Me.cari.Name = "cari"
+        Me.cari.Size = New System.Drawing.Size(253, 20)
+        Me.cari.TabIndex = 0
+        '
+        'cancel
+        '
+        Me.cancel.Image = CType(resources.GetObject("cancel.Image"), System.Drawing.Image)
+        Me.cancel.Location = New System.Drawing.Point(672, 522)
+        Me.cancel.Name = "cancel"
+        Me.cancel.Size = New System.Drawing.Size(100, 27)
+        Me.cancel.TabIndex = 50
+        Me.cancel.Text = "Cancel"
+        '
+        'add_customer_transaksi
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(778, 556)
+        Me.Controls.Add(Me.GroupControl3)
+        Me.Controls.Add(Me.submit)
+        Me.Controls.Add(Me.GroupControl1)
+        Me.Controls.Add(Me.cancel)
+        Me.Name = "add_customer_transaksi"
+        Me.Text = "Daftar Customer"
+        CType(Me.GroupControl3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupControl3.ResumeLayout(False)
+        CType(Me.GridControl1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.GridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.dataview, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.GroupControl1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupControl1.ResumeLayout(False)
+        Me.GroupControl1.PerformLayout()
+        CType(Me.cari.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents GroupControl3 As DevExpress.XtraEditors.GroupControl
+    Friend WithEvents GridControl1 As DevExpress.XtraGrid.GridControl
+    Friend WithEvents GridView1 As DevExpress.XtraGrid.Views.Grid.GridView
+    Friend WithEvents dataview As System.Windows.Forms.DataGridView
+    Friend WithEvents submit As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents GroupControl1 As DevExpress.XtraEditors.GroupControl
+    Friend WithEvents nama As System.Windows.Forms.RadioButton
+    Friend WithEvents id As System.Windows.Forms.RadioButton
+    Friend WithEvents cari As DevExpress.XtraEditors.TextEdit
+    Friend WithEvents cancel As DevExpress.XtraEditors.SimpleButton
+End Class
---
+++ ekspedisi/Fitur tambahan/add_customer_transaksi.vb
@@ -0,0 +1,58 @@
+﻿Public Class add_customer_transaksi 
+
+    Public data As New DataTable
+    Private Sub cancel_Click(sender As Object, e As EventArgs) Handles cancel.Click
+        Me.Close()
+    End Sub
+
+    Private Sub cari_EditValueChanged(sender As Object, e As EventArgs) Handles cari.EditValueChanged
+
+        Try
+            If nama.Checked = True Then
+                GridControl1.DataSource = DtTable("SELECT id_customer_sewa `Kode Customer`, nama_customer `Nama Customer`, Alamat, Email,tel1 `Telepon 1`,tel2 `Telepon 2`,Kota, Provinsi from mcustomer_sewa b where b.`s`='1' and nama_customer like '%" & cari.Text & "%'")
+            Else
+                GridControl1.DataSource = DtTable("SELECT id_customer_sewa `Kode Customer`, nama_customer `Nama Customer`, Alamat, Email,tel1 `Telepon 1`,tel2 `Telepon 2`,Kota, Provinsi from mcustomer_sewa b where b.`s`='1' and id_customer_sewa like '%" & cari.Text & "%'")
+            End If
+
+            For i = 0 To GridView1.Columns.Count - 1
+                GridView1.Columns(i).OptionsColumn.AllowEdit = False
+            Next
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "System Warning", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+
+    End Sub
+
+    Private Sub nama_CheckedChanged(sender As Object, e As EventArgs) Handles nama.CheckedChanged
+        cari_EditValueChanged(sender, e)
+    End Sub
+
+    Private Sub submit_Click(sender As Object, e As EventArgs) Handles submit.Click
+        tambah_transaksi_sewa.customersewa = GridView1.GetRowCellValue(GridView1.FocusedRowHandle, "Kode Customer").ToString
+        tambah_transaksi_sewa.idkaryawan.Text = GridView1.GetRowCellValue(GridView1.FocusedRowHandle, "Nama Customer").ToString
+        tambah_transaksi_sewa.email.Text = GridView1.GetRowCellValue(GridView1.FocusedRowHandle, "Email").ToString
+        tambah_transaksi_sewa.nomortelepon.Text = GridView1.GetRowCellValue(GridView1.FocusedRowHandle, "Telepon 1").ToString & "/" & GridView1.GetRowCellValue(GridView1.FocusedRowHandle, "Telepon 2")
+        tambah_transaksi_sewa.kotaasal.Text = GridView1.GetRowCellValue(GridView1.FocusedRowHandle, "Kota").ToString
+        data = DtTable("SELECT dmaster_customer_sewa.id_mobil `Kode Mobil`,no_pol `No.Polisi`,tipe_mobil `Tipe Mobil`,warna `Warna`,tahun `Tahun`,harga_sewa `Harga Sewa` from mmobil,dmaster_customer_sewa where mmobil.id_mobil=dmaster_customer_sewa.id_mobil and id_customer='" + GridView1.GetRowCellValue(GridView1.FocusedRowHandle, "Kode Customer").ToString + "'")
+        tambah_transaksi_sewa.GridControl1.DataSource = data
+
+        tambah_transaksi_sewa.GridView1.OptionsView.ShowFooter = True
+        For i = 0 To tambah_transaksi_sewa.GridView1.Columns.Count - 1
+            tambah_transaksi_sewa.GridView1.Columns(i).OptionsColumn.AllowEdit = False
+            If tambah_transaksi_sewa.GridView1.Columns(i).FieldName.ToString = "Harga Sewa" Then
+                tambah_transaksi_sewa.GridView1.Columns(i).SummaryItem.SummaryType = DevExpress.Data.SummaryItemType.Sum
+                tambah_transaksi_sewa.GridView1.Columns(i).SummaryItem.FieldName = "Harga Sewa"
+                tambah_transaksi_sewa.GridView1.Columns(i).SummaryItem.DisplayFormat = "Total =    Rp. {0:N2} "
+            End If
+        Next
+        Me.Close()
+    End Sub
+
+    Private Sub GridControl1_DoubleClick(sender As Object, e As EventArgs) Handles GridControl1.DoubleClick
+        submit_Click(sender, e)
+    End Sub
+
+    Private Sub add_customer_transaksi_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        cari_EditValueChanged(sender, e)
+    End Sub
+End Class
